3b3d6bd29e8bb5be260a7a149c6ac5698af4bd86
forest <bogdadark@gmail.com>
2024-09-24 18:20:41 +0300

    zrobyly_ta_zminyly
    wise notes just bc

--- ishdu_hu/pupils_small.py
+++ ishdu_hu/pupils_small.py
@@ -1,7 +1,9 @@
 class Pupil:
-    def __init__(self, surname, name, mark):
+    def __init__(self, gender, surname, name, land, mark):
+        self.gender = gender
         self.surname = surname
         self.name = name
+        self.land = land
         self.mark = mark
 
 pupil_amount = 0
@@ -11,7 +13,7 @@ sum = 0
 pupils = []
 def print_class(pupils):
     for pupil in pupils:
-        print(pupil.surname, pupil.name, "-", pupil.mark)
+        print(pupil.gender, pupil.surname, pupil.name, "from", pupil.land, "-", pupil.mark)
 
 def print_ten(pupils):
     print("Best students")
@@ -23,7 +25,7 @@ def print_ten(pupils):
 with open("pupil.txt", "r", encoding= "UTF-8") as file:
     for line in file:
         data = line.split(" ")
-        pupil = Pupil(data[0], data[1], int(data[2]))
+        pupil = Pupil(data[0], data[1], data[2], data[3], int(data[4]))
         pupils.append(pupil)
 
-print_ten(pupils)
+print_class(pupils)
---
+++ wise_notes/maine.py
@@ -0,0 +1,14 @@
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QMainWindow
+from ui import Ui_MainWindow
+
+class Widget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+app = QApplication([])
+ex = Widget()
+ex.show()
+app.exec_()
