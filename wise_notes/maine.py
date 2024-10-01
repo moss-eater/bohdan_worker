0a7728f99727f4ccb3b49447fe2cb127d0ee27d7
forest <bogdadark@gmail.com>
2024-10-01 18:01:44 +0300

    shidd

--- wise_notes/maine.py
+++ wise_notes/maine.py
@@ -1,6 +1,12 @@
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog
 from PyQt5.QtWidgets import QMainWindow
 from ui import Ui_MainWindow
+import json
+
+#note_text, note_list, tag_list
+#note_create, note_delete, note_save
+#tag_list_add, tag_list_unpin, tag_list_search
+#line_tag_edit
 
 class Widget(QMainWindow):
     def __init__(self):
@@ -8,7 +14,50 @@ class Widget(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-app = QApplication([])
-ex = Widget()
-ex.show()
-app.exec_()
+        self.notes = {
+            "G'day sir!": {
+                "taxt" : "Shitteen",
+                "tegs" : ["wan", "tu"]
+            }
+        }
+        self.ui.note_list.itemClicked.connect(self.show_note)
+        self.load_notes() # reads text
+
+        def show_note(self):
+            key = self.ui.note_list.selectItems()[0].text()
+            self.ui.note_text.setText(self.notes[key]["taxt"])
+            self.ui.tag_list.clear()
+            self.ui.tag_list.addItems(self.notes[key]["tegs"])
+
+        def load_notes(self):
+            try:
+                with open("notes_data.json", "r") as file:
+                    self.notes = json.load(file)
+                self.ui.list_notes.addItems(self.notes)
+            except:
+                with open("notes_data.json", "w") as file:
+                    json.dump(self.notes, file)
+
+        def add_note(self):
+            self.note_name, ok = QInputDialog.getText(self.notes_win, "Add", "Name:")
+            if ok and self.note_name != "":
+                self.notes[self.note_name] = {"taxt" : "", "tegs" : []}
+                self.ui.note_list.addItem(self.note_name)
+                self.ui.tag_list.addItems(self.notes[self.note_name]["tegs"])
+                print(self.notes)
+
+        def save_note(self):
+            if self.ui.note_list.selectedItems():
+                self.key = self.ui.note_list.selectedItems()[0].text()
+                self.notes[self.key]["taxt"] = self.ui.note_text.toPlainText()
+                with open("notes_data.json", "w")as file:
+                    json.dump(self.notes, self.file, sort_keys = True, ensure_ascii = False)
+                print(self.notes)
+            else:
+                print('no')
+
+if __name__ == "__main__":
+    app = QWidget.QApplication([])
+    ex = Widget()
+    ex.show()
+    app.exec_()
