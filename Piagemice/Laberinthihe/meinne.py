a67cd2379e8fb1b70e82b24a82ab5aff76e9441f
forest <bogdadark@gmail.com>
2024-12-24 18:13:35 +0200

    der gamisch

--- Piagemice/Laberinthihe/meinne.py
+++ Piagemice/Laberinthihe/meinne.py
@@ -8,6 +8,8 @@ HEIGHT = 600
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Laberinthihe")
 bg = pygame.transform.scale(pygame.image.load('./Laberinthihe/tloes.png'), (WIDTH, HEIGHT))
+playinhu = pygame.mixer.music.load('./Laberinthihe/Boiling-Mine-OST-Stone-Story-RPG.ogg')
+pygame.mixer.music.play()
 
 clock = pygame.time.Clock()
 
@@ -24,26 +26,78 @@ class GameSprite(pygame.sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
+class Palehin(GameSprite):
+    def __init__(self, palehin_image, palehin_har, palehin_jel, palehin_sweh):
+        super().__init__(palehin_image, palehin_har, palehin_jel, palehin_sweh)
 
-palehin = GameSprite('./Laberinthihe/gravienker.png', 100, HEIGHT - 150, 4)
-gigurandes_iho = GameSprite('./Laberinthihe/giaghenti.png', WIDTH - 170, 300, 2)
-gigurandes_ej = GameSprite('./Laberinthihe/giaghenti.png', WIDTH - 1000, 100, 3)
+    def update(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[pygame.K_d] and self.rect.x < 1150:
+            self.rect.x += self.speed
+        if keys[pygame.K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[pygame.K_s] and self.rect.y < 550:
+            self.rect.y += self.speed
+
+class Gigurandes(GameSprite):
+    def __init__(self, palehin_image, palehin_har, palehin_jel, palehin_sweh):
+        super().__init__(palehin_image, palehin_har, palehin_jel, palehin_sweh)
+
+    direction1 = 'left'
+    direction = 'left'
+
+    def update_1(self):
+        if self.rect.x <= (WIDTH - 300):
+            self.direction1 = 'right'
+        if self.rect.x >= (WIDTH - 100):
+            self.direction1 = 'left'
+
+        if self.direction1 == 'left':
+            self.rect.x -= self.speed
+        if self.direction1 == 'right':
+            self.rect.x += self.speed
+
+    def update_2(self):
+        if self.rect.x <= (WIDTH - 1050):
+            self.direction = 'right'
+        if self.rect.x >= (WIDTH - 650):
+            self.direction = 'left'
+
+        if self.direction == 'left':
+            self.rect.x -= self.speed
+        if self.direction == 'right':
+            self.rect.x += self.speed
+
+palehin = Palehin('./Laberinthihe/gravienker.png', 100, HEIGHT - 150, 4)
+gigurandes_ih = Gigurandes('./Laberinthihe/giaghenti.png', WIDTH - 170, 300, 3)
+gigurandes_ej = Gigurandes('./Laberinthihe/giaghenti.png', WIDTH - 1000, 100, 2)
 sahurdaes = GameSprite('./Laberinthihe/der_prizen.png', WIDTH - 170, HEIGHT - 170, 0)
 
 
-game_over = False
-while not game_over:
-    window.fill((0, 0, 0))
-    window.blit(bg, (0, 0))
-
-    palehin.reset()
-    gigurandes_iho.reset()
-    gigurandes_ej.reset()
-    sahurdaes.reset()
-    
+game = True
+finish = False
+while game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            game_over = True
+            game = False
+    if finish != True:
+        window.fill((0, 0, 0))
+        window.blit(bg, (0, 0))
+
+        palehin.reset()
+        palehin.update()
+
+        gigurandes_ih.reset()
+        gigurandes_ih.update_1()
+
+        gigurandes_ej.reset()
+        gigurandes_ej.update_2()
+
+        sahurdaes.reset()
+
+        #when palehin toucher sahurdaes do pygame.mixer.music.stop(playinhu)
 
     pygame.display.update()
     clock.tick(60)
